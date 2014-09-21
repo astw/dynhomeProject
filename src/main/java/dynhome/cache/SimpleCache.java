a22a121eb14bcfe46c4e18ef3a92a1a32b5a9913
Shuhao <wshuhao@gmail.com>
2014-09-21 00:29:41 -0700

    ca

---
+++ src/main/java/dynhome/cache/SimpleCache.java
@@ -0,0 +1,34 @@
+package dynhome.cache;
+
+import dynhome.entity.Identity;
+import dynhome.interfaces.IIdentity;
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.CacheManager; 
+import net.sf.ehcache.Element;
+
+public class SimpleCache {
+
+    CacheManager singletonManager = CacheManager.create();
+    Cache memoryOnlyCache = new Cache("IentityCache", 5000, false, false, 5, 2);
+    Cache identityCache = singletonManager.getCache("IentityCache"); 
+    
+    public SimpleCache(){ 
+        singletonManager.addCache (memoryOnlyCache); 
+    }
+    
+    public void Put(String key, IIdentity identity)
+    { 
+        identityCache.put(new Element(key, identity)); 
+    }
+    
+    public IIdentity Get(String key)
+    { 
+        Element ele = identityCache.get(key);
+        if(ele != null)
+        { 
+            ele.getValue();
+        }
+        return null;
+    }
+    
+}
--- src/main/java/dynhome/rest/UserResource.java
+++ src/main/java/dynhome/rest/UserResource.java
@@ -1,9 +1,11 @@
 package dynhome.rest;
 
 import dynhome.cache.IdentityCache;
+import dynhome.cache.SimpleCache;
 import dynhome.entity.Identity;
 import dynhome.interfaces.IIdentity;
 import dynhome.interfaces.IIdentityDao;
+import java.util.concurrent.ExecutionException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -12,21 +14,26 @@ import javax.ws.rs.core.MediaType;
 @Path("/users")
 public class UserResource {
 
-    IIdentityDao  identityDao; 
-    IdentityCache identityCache ;
+    IIdentityDao identityDao;
+    SimpleCache identityCache;
+
     public void setIdentityDao(IIdentityDao identityDao) {
         this.identityDao = identityDao;
-       identityCache = new IdentityCache();
-       identityCache.setIdentityDao(identityDao);
-    } 
-    
+        identityCache = new SimpleCache();
+
+    }
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public IIdentity getDefaultIdentityInJSON() { 
+    public IIdentity getDefaultIdentityInJSON() throws ExecutionException {
         //return identityDao.getIdentity();
-        IIdentity identity = identityCache.getByEmail("wsh@13s.com");
-        if(identity == null) 
-            return identityDao.getIdentity();
+        //IIdentity identity = identityCache.getByEmail("wsh@13s.com");  
+        String key = "wsh@13s.com";
+        IIdentity identity = identityCache.Get(key);
+        if (identity == null) {
+            identity = identityDao.getIdentity();
+            identityCache.Put(key, identity);
+        }
         return identity;
     }
 }
