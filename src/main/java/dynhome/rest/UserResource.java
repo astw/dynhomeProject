38463757b9af91fdd119ae1744bf92df23d514f4
Shuhao Wang <wshuhao@163.com>
2014-09-20 23:52:16 -0700

    cache

--- src/main/java/dynhome/rest/UserResource.java
+++ src/main/java/dynhome/rest/UserResource.java
@@ -1,5 +1,7 @@
 package dynhome.rest;
 
+import dynhome.cache.IdentityCache;
+import dynhome.entity.Identity;
 import dynhome.interfaces.IIdentity;
 import dynhome.interfaces.IIdentityDao;
 import javax.ws.rs.GET;
@@ -11,14 +13,20 @@ import javax.ws.rs.core.MediaType;
 public class UserResource {
 
     IIdentityDao  identityDao; 
-
+    IdentityCache identityCache ;
     public void setIdentityDao(IIdentityDao identityDao) {
         this.identityDao = identityDao;
+       identityCache = new IdentityCache();
+       identityCache.setIdentityDao(identityDao);
     } 
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public IIdentity getDefaultIdentityInJSON() { 
-        return identityDao.getIdentity();
+        //return identityDao.getIdentity();
+        IIdentity identity = identityCache.getByEmail("wsh@13s.com");
+        if(identity == null) 
+            return identityDao.getIdentity();
+        return identity;
     }
 }
