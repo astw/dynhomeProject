81209e62505075d9e458a205dea73eeaf90622d6
Shuhao <wshuhao@gmail.com>
2014-12-18 14:18:43 -0800

    trying to use JpaRepository

--- src/main/java/dynhome/rest/UserResource.java
+++ src/main/java/dynhome/rest/UserResource.java
@@ -1,39 +1,41 @@
 package dynhome.rest;
 
-import dynhome.cache.IdentityCache;
 import dynhome.cache.SimpleCache;
 import dynhome.entity.Identity;
-import dynhome.interfaces.IIdentity;
 import dynhome.interfaces.IIdentityDao;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Path("/users")
 public class UserResource {
 
+    //@Autowired
     IIdentityDao identityDao;
-    SimpleCache identityCache;
+  //  SimpleCache identityCache;
 
     public void setIdentityDao(IIdentityDao identityDao) {
         this.identityDao = identityDao;
-        identityCache = new SimpleCache();
+  //      identityCache = new SimpleCache();
 
     }
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public IIdentity getDefaultIdentityInJSON() throws ExecutionException {
-        //return identityDao.getIdentity();
+    public List<Identity> getDefaultIdentityInJSON() throws ExecutionException {
+        //return identityDao.findAll();
+        return identityDao.getIdentities();
         //IIdentity identity = identityCache.getByEmail("wsh@13s.com");  
-        String key = "wsh@13s.com";
-        IIdentity identity = identityCache.Get(key);
-        if (identity == null) {
-            identity = identityDao.getIdentity();
-            identityCache.Put(key, identity);
-        }
-        return identity;
+//        String key = "wsh@13s.com";
+//        IIdentity identity = identityCache.Get(key);
+//        if (identity == null) {
+//            identity = identityDao.getIdentity();
+//            identityCache.Put(key, identity);
+//        }
+//        return identity;
     }
 }
